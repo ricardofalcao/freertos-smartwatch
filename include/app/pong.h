b04168d2f83b16e31f3abe5b382149cedceaed52
Ricardo Falcão <ricardomanuelfalcao20@gmail.com>
2021-05-21 19:17:10 +0100

    changes to pong collision and graphics

--- include/app/pong.h
+++ include/app/pong.h
@@ -5,19 +5,27 @@
 
 #include "app/app.h"
 
-
 class App_Pong : public App {
     private:
         int tick = 0;
+        int deadTime = 0;
+
+        int bot_score;
+        float bot_max_speed = 5;
+        int bot_tick_ms = 10;
+
+        int player_score;
         
-        int playerx = DEFAULT_VIEWPORT.width / 2, tplayerx = DEFAULT_VIEWPORT.width / 2;
+        float playerx, tplayerx;
+        float botx;
 
-        int botx = DEFAULT_VIEWPORT.width / 2;
+        float ballx, bally;
+        float ballvx, ballvy;
 
-        int ballx = DEFAULT_VIEWPORT.width / 2, bally = DEFAULT_VIEWPORT.height / 2;
+        char _bot_score[12];
+        char _player_score[12];
 
-        char _bot_score[3];
-        char _player_score[3];
+        uint8_t state;
 
     public:
         App_Pong();
@@ -29,13 +37,21 @@ class App_Pong : public App {
         void onClose() override;   
     
     private:
+        void setup_game();
+
+        void reset_game();
+
+        void update_game();
+
         void print_top_bar(GBatch_t * batch, int previous_x, int new_x);
 
         void print_bottom_bar(GBatch_t * batch, int previous_x, int new_x);
 
+        void print_middle_lines(GBatch_t * batch, uint32_t color);
+
         void print_field_lines(GBatch_t * batch, uint32_t color);
 
-        void print_ball(GBatch_t * batch, int x_center, int y_center,int previous_x, int previous_y, uint32_t color);
+        void print_ball(GBatch_t * batch, int x_center, int y_center, int previous_x_center, int previous_y_center);
 
         void print_score(GBatch_t * batch, uint32_t color);
 };
--- src/app/pong.cpp
+++ src/app/pong.cpp
@@ -4,13 +4,12 @@
 #include "touch.h"
 #include "lang/lang.h"
 
+#define TIMESTEP_MS             10
+
+#define PONG_DEFAULT_BALL_VELOCITY    5
+
 #define BACKGROUND_COLOR        TFT_BLACK
 #define GAME_COLOR              TFT_WHITE
-
-#define GAME_X_LEFT             35
-#define GAME_X_RIGHT            DEFAULT_VIEWPORT.width-35
-#define GAME_Y_DOWN             35
-#define GAME_Y_UP               DEFAULT_VIEWPORT.height-35
 
 #define MARGIN_Y                35
 #define MARGIN_X                35
@@ -24,42 +23,36 @@
 #define SPACE_BETWEEN_BARS      6
 #define DASHED_BARS_LENGTH      10
 
-#define SET_VELOCITY            1
+//
 
-#define BALL_RADIUS             9
+#define STATE_PLAY              0
+#define STATE_WIN               1
 
-int bot_score = 0;
-int player_score = 0;
+//
 
-int end;
-int vx;
-int vy;
-int rcenter;
-float cx;      // circle position (set with mouse)
-float cy;
-float r;      // circle radius
-int nbotx;
-int size[2];
+#define BALL_RADIUS             6
 
-void setup_game(){
-    r=BALL_RADIUS;
-    end=0;
-    vx=SET_VELOCITY;
-    vy=SET_VELOCITY;
-    nbotx=0;
-    size[1]=GAME_X_RIGHT-GAME_X_LEFT;
-    size[2]=GAME_Y_UP-GAME_Y_DOWN;
-    cx=size[1]/2+MARGIN_X;
-    cy=size[2]/2+MARGIN_Y;
-    
+#define MAX_BOUNCE_ANGLE        0.8726646259971648f
 
+bool _circle_intersects_rect(float cx, float cy, float radius, float rx, float ry, float rw, float rh) {
+    float distX = abs(cx - rx-rw/2);
+    float distY = abs(cy - ry-rh/2);
+    if (distX > (rw/2 + radius)) { return false; }
+    if (distY > (rh/2 + radius)) { return false; }
+    if (distX <= (rw/2)) { return true; } 
+    if (distY <= (rh/2)) { return true; }
+    float dx=distX-rw/2;
+    float dy=distY-rh/2;
+    return (dx*dx+dy*dy<=(radius*radius));
+}
+
+
+float _lerp(float a, float b, float x)
+{ 
+  return a + x * (b - a);
 }
 
 void App_Pong::print_top_bar(GBatch_t * batch, int previous_x, int new_x) {
-    if (new_x == previous_x) {
-        return;
-    }
-
     if (new_x > previous_x) {
         batch->fillRectangle(previous_x - BAR_HALF_LENGTH, MARGIN_Y, new_x - previous_x, BAR_HEIGHT, BACKGROUND_COLOR);
     } else {
@@ -71,10 +64,6 @@ void App_Pong::print_top_bar(GBatch_t * batch, int previous_x, int new_x) {
 }
 
 void App_Pong::print_bottom_bar(GBatch_t * batch, int previous_x, int new_x) {
-    if (new_x == previous_x) {
-        return;
-    }
-
     if (new_x > previous_x) {
         batch->fillRectangle(previous_x - BAR_HALF_LENGTH, batch->viewHeight() - MARGIN_Y - BAR_HEIGHT, new_x - previous_x, BAR_HEIGHT, BACKGROUND_COLOR);
     } else {
@@ -84,16 +73,19 @@ void App_Pong::print_bottom_bar(GBatch_t * batch, int previous_x, int new_x) {
     batch->fillRectangle(new_x - BAR_HALF_LENGTH, batch->viewHeight() - MARGIN_Y - BAR_HEIGHT, BAR_LENGTH, BAR_HEIGHT, GAME_COLOR);
 }
 
-void App_Pong::print_field_lines(GBatch_t * batch, uint32_t color) {
+void App_Pong::print_middle_lines(GBatch_t * batch, uint32_t color) {
     int numberOfLines = (batch->viewWidth() - DASHED_BARS_LENGTH) / (DASHED_BARS_LENGTH + SPACE_BETWEEN_BARS) + 1;
     int offsetX = ((batch->viewWidth() - DASHED_BARS_LENGTH) % (DASHED_BARS_LENGTH + SPACE_BETWEEN_BARS)) / 2;
 
     for(int i = 0; i < numberOfLines; i++) {
         int x = offsetX + i * (DASHED_BARS_LENGTH+SPACE_BETWEEN_BARS);
 
-        batch->drawLine(x, batch->viewHeight() / 2, x + DASHED_BARS_LENGTH, batch->viewHeight() / 2, color, 2);
+        batch->fillRectangle(x, batch->viewHeight() / 2 - 1, DASHED_BARS_LENGTH, 3, color);
     }
-    
+
+}
+
+void App_Pong::print_field_lines(GBatch_t * batch, uint32_t color) {
     batch->drawLine(1, 1, 1, batch->viewHeight(), color, 8);
     batch->drawLine(batch->viewWidth(), 1, batch->viewWidth(), batch->viewHeight(), color, 8);
 
@@ -101,70 +93,18 @@ void App_Pong::print_field_lines(GBatch_t * batch, uint32_t color) {
 
 void App_Pong::print_score(GBatch_t * batch, uint32_t color) {
 
-    sprintf(_bot_score, "%d", bot_score);
-    batch->drawString(batch->viewWidth() - MARGIN_X, batch->viewHeight()/2 - MARGIN_Y, _bot_score, color, 4, MC_DATUM);
+    sprintf(_bot_score, " %d ", bot_score);
+    batch->drawFilledString(batch->viewWidth() - MARGIN_X, batch->viewHeight()/2 - MARGIN_Y, _bot_score, color, BACKGROUND_COLOR, 4, MC_DATUM);
     
-    sprintf(_player_score, "%d", player_score);
-    batch->drawString(batch->viewWidth() - MARGIN_X, batch->viewHeight()/2 + MARGIN_Y, _player_score, color, 4, MC_DATUM);
-
+    sprintf(_player_score, " %d ", player_score);
+    batch->drawFilledString(batch->viewWidth() - MARGIN_X, batch->viewHeight()/2 + MARGIN_Y, _player_score, color, BACKGROUND_COLOR, 4, MC_DATUM);
 }
 
-void App_Pong::print_ball(GBatch_t * batch, int x_center, int y_center,int previous_x, int previous_y, uint32_t color) {
-    batch->fillRectangle(previous_x,previous_y, r, r, BACKGROUND_COLOR);
-    batch->fillCircle(x_center, y_center, BALL_RADIUS, color);
+void App_Pong::print_ball(GBatch_t * batch, int x_center, int y_center, int previous_x_center, int previous_y_center) {
+    batch->fillCircle(previous_x_center, previous_y_center, BALL_RADIUS, BACKGROUND_COLOR);
+    batch->fillCircle(x_center, y_center, BALL_RADIUS, GAME_COLOR);
 }
 
-bool circleRect(float cx, float cy, float radius, float rx, float ry, float rw, float rh) {
-
-  // temporary variables to set edges for testing
-  float testX = cx;
-  float testY = cy;
-
-  // which edge is closest?
-  if (cx < rx)         testX = rx;      // test left edge
-  else if (cx > rx+rw) testX = rx+rw;   // right edge
-  if (cy < ry)         testY = ry;      // top edge
-  else if (cy > ry+rh) testY = ry+rh;   // bottom edge
-
-  // get distance from closest edges
-  float distX = cx-testX;
-  float distY = cy-testY;
-  float distance = sqrt( (distX*distX) + (distY*distY) );
-
-  // if the distance is less than the radius, collision!
-  if (distance <= radius) {
-    return true;
-  }
-  return false;
-}
-
-void update_game(float cx, float cy, float r, float plx, float botx,int vx, int vy){
-    int cxa=cx;
-    int cya=cy;
-    cxa=cxa+vx;
-    cya=cya+vy;
-     if (cxa+r>size[1]+MARGIN_X || cxa-r<MARGIN_X){
-       
-        vx=-vx;
-        }
-  
-    bool colision_player=circleRect(cxa,cya,r,plx-BAR_HALF_LENGTH,BAR_HEIGHT,BAR_LENGTH,BAR_HEIGHT);
-    bool colision_bot=circleRect(cxa,cya,r,botx-BAR_HALF_LENGTH,BAR_HEIGHT,BAR_LENGTH,BAR_HEIGHT);
-    
-  
-    if(colision_bot==1 || colision_bot==1){
-        vy=-vy;    
-    }
-
-     cy=cy+vy;
-     cx=cx+vx;
-
-     if(cy-r>size[2] || cy+r<0)
-     end=1;
-
-     return;
-        
-}
 
 App_Pong::App_Pong() : App(MSG_PONG_NAME, MSG_PONG_DESCRIPTION) {
     priority = 3;
@@ -172,64 +112,183 @@ App_Pong::App_Pong() : App(MSG_PONG_NAME, MSG_PONG_DESCRIPTION) {
     canMinimize = false;
 }
 
- 
 void App_Pong::onOpen() {
-    bot_score=0;
-    player_score=0;
-    GBatch_t batch = graphics.beginBatch(DEFAULT_VIEWPORT);
-    batch.fillScreen(BACKGROUND_COLOR);
-    print_field_lines(&batch, GAME_COLOR);
-    print_score(&batch, GAME_COLOR);
-    graphics.endBatch(&batch);
     setup_game();
-
 }
 
-float lerp(float a, float b, float x)
-{ 
-  return a + x * (b - a);
+ 
+void App_Pong::setup_game() {
+    bot_score = 0;
+    player_score = 0;
+
+    playerx = DEFAULT_VIEWPORT.width / 2;
+    tplayerx = playerx;
+
+    botx = DEFAULT_VIEWPORT.width / 2;
+
+    ballx = DEFAULT_VIEWPORT.width / 2;
+    bally = DEFAULT_VIEWPORT.height / 2;
+
+    float angle = random(0, 360) * PI / 180;
+    ballvx = PONG_DEFAULT_BALL_VELOCITY * cos(angle);
+    ballvy = PONG_DEFAULT_BALL_VELOCITY * sin(angle);
+
+    state = STATE_PLAY;
+
+    GBatch_t batch = graphics.beginBatch(DEFAULT_VIEWPORT);
+    batch.fillScreen(BACKGROUND_COLOR);
+    print_middle_lines(&batch, GAME_COLOR);
+    print_field_lines(&batch, GAME_COLOR);
+    print_score(&batch, GAME_COLOR);
+    print_top_bar(&batch, botx, botx);
+    print_bottom_bar(&batch, playerx, playerx);
+    graphics.endBatch(&batch);
+
+    deadTime = 1000 / TIMESTEP_MS;
+}
+
+ 
+void App_Pong::reset_game() {
+    int oballx = ballx;
+    int obally = bally;
+
+    ballx = DEFAULT_VIEWPORT.width / 2;
+    bally = DEFAULT_VIEWPORT.height / 2;
+
+    float angle = random(0, 360) * PI / 180;
+    ballvx = PONG_DEFAULT_BALL_VELOCITY * cos(angle);
+    ballvy = PONG_DEFAULT_BALL_VELOCITY * sin(angle);
+
+    state = STATE_PLAY;
+
+    GBatch_t batch = graphics.beginBatch(DEFAULT_VIEWPORT);
+    print_middle_lines(&batch, GAME_COLOR);
+    print_field_lines(&batch, GAME_COLOR);
+    print_score(&batch, GAME_COLOR);
+    print_ball(&batch, ballx, bally, oballx, obally);
+    print_top_bar(&batch, botx, botx);
+    print_bottom_bar(&batch, playerx, playerx);
+    graphics.endBatch(&batch);
+
+    deadTime = 1000 / TIMESTEP_MS;
+}
+
+void App_Pong::update_game(){
+    int nballx= ballx + ballvx;
+    int nbally = bally + ballvy;
+
+    if (_circle_intersects_rect(nballx, nbally, BALL_RADIUS, -5, -5, 6, DEFAULT_VIEWPORT.height) || _circle_intersects_rect(nballx, nbally, BALL_RADIUS, DEFAULT_VIEWPORT.width - 2, 0, 6, DEFAULT_VIEWPORT.height)) {
+        ballvx = -ballvx;
+    }
+
+    if (_circle_intersects_rect(nballx, nbally, BALL_RADIUS, playerx - BAR_HALF_LENGTH, DEFAULT_VIEWPORT.height - MARGIN_Y - BAR_HEIGHT, BAR_LENGTH, BAR_HEIGHT)) {
+        float relativeIntersectX = playerx - ballx;
+        float normalizedRelativeIntersectionX = relativeIntersectX / (BAR_LENGTH / 2);
+        float bounceAngle = normalizedRelativeIntersectionX * MAX_BOUNCE_ANGLE;
+
+        ballvy = PONG_DEFAULT_BALL_VELOCITY*-cos(bounceAngle);
+        ballvx = PONG_DEFAULT_BALL_VELOCITY*-sin(bounceAngle);
+
+    } else if (_circle_intersects_rect(nballx, nbally, BALL_RADIUS, botx - BAR_HALF_LENGTH, MARGIN_Y, BAR_LENGTH, BAR_HEIGHT))  {
+        float relativeIntersectX = botx - ballx;
+        float normalizedRelativeIntersectionX = relativeIntersectX / (BAR_LENGTH / 2);
+        float bounceAngle = normalizedRelativeIntersectionX * MAX_BOUNCE_ANGLE;
+
+        ballvy = PONG_DEFAULT_BALL_VELOCITY*cos(bounceAngle);
+        ballvx = PONG_DEFAULT_BALL_VELOCITY*-sin(bounceAngle);
+
+    }
+
+    ballx += ballvx;
+    bally += ballvy;
+
+    if (bally < MARGIN_Y) {
+        state = STATE_WIN;
+
+        player_score++;
+    } else if (bally > (DEFAULT_VIEWPORT.height - MARGIN_Y)) {
+        state = STATE_WIN;
+
+        bot_score++;
+    }
 }
 
 void App_Pong::onTick() {
-    tick++;
+    switch (state) {
+        case STATE_PLAY: {
+            int cballx = ballx;
+            int cbally = bally;
 
-    if(end==1){
-        player_score=1;
-        setup_game();
-    }
+            if (deadTime == 0) {
+                update_game();
+            } else {
+                deadTime--;
+            }
 
-    nbotx = ((int) (sin(0.05 * tick) * (DEFAULT_VIEWPORT.width / 2 - BAR_LENGTH))) + DEFAULT_VIEWPORT.width / 2;
+            TouchData data = touch.get();
 
-    // Draw top bar
-    GBatch_t batch = graphics.beginBatch(DEFAULT_VIEWPORT);
-    if (nbotx != botx) {
-        print_top_bar(&batch, botx, nbotx);
-        botx = nbotx;
-    }
-  
-    TouchData data = touch.get();
+            if (data.pressed) {
+                int nplayerx = constrain(data.x, BAR_HALF_LENGTH + 5, DEFAULT_VIEWPORT.width - BAR_HALF_LENGTH - 5);
 
-    if (data.pressed) {
-        int nplayerx = constrain(data.x, BAR_HALF_LENGTH + 5, DEFAULT_VIEWPORT.width - BAR_HALF_LENGTH - 5);
+                if (playerx != nplayerx) {
+                    tplayerx = nplayerx;
+                }
+            }
 
-        if (playerx != nplayerx) {
-            tplayerx = nplayerx;
+            // Draw bot bar
+            GBatch_t batch = graphics.beginBatch(DEFAULT_VIEWPORT);
+
+            if (cballx != ballx || cbally != bally) {
+                print_ball(&batch, ballx, bally, cballx, cbally);
+            }
+
+            float iiplayerx = ceil(_lerp(playerx, tplayerx, 0.3));
+            
+            if (playerx != iiplayerx || cbally > DEFAULT_VIEWPORT.height - MARGIN_Y - BAR_HEIGHT - BALL_RADIUS) {
+                print_bottom_bar(&batch, playerx, iiplayerx);
+            }
+
+            playerx = iiplayerx;
+
+            float tempbotx = botx;
+            
+            if (tick % (bot_tick_ms / TIMESTEP_MS) == 0) {
+                if (tempbotx < ballx - 3) {
+                    tempbotx = min(tempbotx + bot_max_speed, ballx);
+                } else if (tempbotx > ballx + 3) {
+                    tempbotx = max(tempbotx - bot_max_speed, ballx);
+                }
+            }
+
+            float nbotx = constrain(tempbotx, BAR_HALF_LENGTH + 5, DEFAULT_VIEWPORT.width - BAR_HALF_LENGTH - 5);
+
+            // Draw top bar
+            if (nbotx != botx || cbally < MARGIN_Y + BAR_HEIGHT + BALL_RADIUS) {
+                print_top_bar(&batch, botx, nbotx);
+                botx = nbotx;
+            }
+
+            graphics.endBatch(&batch);
+
+            
+            if (tick % (100 / TIMESTEP_MS) == 0) {
+                batch = graphics.beginBatch(DEFAULT_VIEWPORT);
+                print_score(&batch, GAME_COLOR);
+                print_middle_lines(&batch, GAME_COLOR);
+                graphics.endBatch(&batch);
+            Serial.print(1);
+            }
+
+            tick++;
+            vAppDelay(TIMESTEP_MS / portTICK_PERIOD_MS);
+            break;
+        }
+
+        case STATE_WIN: {
+            reset_game();
+            break;
         }
     }
-
-    // Draw bot bar
-    int iiplayerx = ceil(lerp(playerx, tplayerx, 0.3));
-    print_bottom_bar(&batch, playerx, iiplayerx);
-    playerx = iiplayerx;
-    int cxa=cx;
-    int cya=cy;
-
-    update_game(cx,cy,r,tplayerx,nbotx,vx,vy);
-    print_ball(&batch,cx,cy,cxa,cya,GAME_COLOR);
-
-    graphics.endBatch(&batch);
-
-    vAppDelay(10 / portTICK_PERIOD_MS);
 }
 
 void App_Pong::onClose() {
